87db141672a68e42da36c3dd26a22597831a371d
KAZUKI27854 <02kazuki16@gmail.com>
2021-02-27 10:58:27 +0000

    temporary saving

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,22 @@
 class UsersController < ApplicationController
  
  def show
-     @books = Book.all
+  @books = Book.all
  end
  
+ def edit
+  @user = User.find_by(id: current_user.id)
+ end
+ 
+ def update
+  @user = User.find(params[:id])
+  @user.update(user_params)
+  redirect_to user_path
+ end
+ 
+ private
+ def user_params
+  params.require(:user).permit(:name, :introduction)
+ end
  
 end
