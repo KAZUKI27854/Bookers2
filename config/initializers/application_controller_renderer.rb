7e4c590964994477d62a909101c32f3160c496bc
KAZUKI27854 <02kazuki16@gmail.com>
2021-03-02 01:10:06 +0000

    temporary saving

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9fad77680aa918901cd1b272f38836a88cbf3a480251f74b668861892ccd3a6cdb48195754fc7c61cdae2e05ac38a55596e03486c739792a990686f0b272a564'
