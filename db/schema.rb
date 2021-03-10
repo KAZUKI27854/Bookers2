be48004358c0ddad31172328b83d3234ddae1e29
KAZUKI27854 <02kazuki16@gmail.com>
2021-03-10 09:33:05 +0000

    modified

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_065657) do
+ActiveRecord::Schema.define(version: 2021_03_10_055335) do
 
   create_table "books", force: :cascade do |t|
     t.text "title"
