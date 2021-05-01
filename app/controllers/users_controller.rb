f8d8e7dd8ed41b06207f525443339d9e6ec44a83
KAZUKI27854 <02kazuki16@gmail.com>
2021-05-01 13:53:44 +0000

    create follower system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,25 @@
 class UsersController < ApplicationController
- 
+
  def index
   @book = Book.new
   @users = User.all
   @user = current_user
  end
- 
+
  def show
   @book = Book.new
   @user = User.find(params[:id])
   @posted_book = Book.where(user_id: params[:id])
   @books = @posted_book.all
  end
- 
+
  def edit
   @user = User.find(params[:id])
   unless @user.id == current_user.id
    redirect_to user_path(current_user.id)
   end
  end
- 
+
  def update
   @user = User.find(params[:id])
   if @user.update(user_params)
@@ -29,10 +29,20 @@ class UsersController < ApplicationController
      render "edit"
   end
  end
- 
+
+ def followers_index
+  user = User.find(params[:id])
+  @users = user.following_user.where.not(id: user.id)
+ end
+
+ def followed_index
+  user = User.find(params[:id])
+  @users = user.follower_user.where.not(id: user.id)
+ end
+
  private
  def user_params
   params.require(:user).permit(:name, :introduction, :profile_image)
  end
- 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get '/home/about' => 'homes#about'
+  post 'follow/:id' => 'relationships#follow', as: 'follow'
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow'
+  get 'users/:id/followers_index', to: 'users#followers_index', as: 'followers'
+  get 'users/:id/followed_index', to: 'users#followed_index', as: 'followed'
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :edit, :update]
+  resources :relationships, only: [:create, :destroy]
 end
