8986f2837826916a5b4998d4e9fcbe51e496bf96
KAZUKI27854 <02kazuki16@gmail.com>
2021-05-01 09:27:42 +0000

    add favorite and comment system

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
   def new
   end
-  
+
   def create
     @user = current_user
     @book = Book.new(book_params)
@@ -14,26 +14,28 @@ class BooksController < ApplicationController
        render "index"
     end
   end
-  
+
   def index
     @book = Book.new
     @books = Book.all
     @user = current_user
   end
-  
+
   def show
     @book = Book.new
     @book_show = Book.find(params[:id])
     @user = User.find(@book_show.user_id)
+    @book_comment = BookComment.new
+    @book_comments = BookComment.where(book_id: @book_show.id)
   end
-  
+
   def edit
     @book = Book.find(params[:id])
-    unless current_user == @book.user 
+    unless current_user == @book.user
       redirect_to books_path
     end
   end
-  
+
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
@@ -43,17 +45,17 @@ class BooksController < ApplicationController
        render "edit"
     end
   end
-  
+
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
   end
-  
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   attachment :profile_image
-  
+
   validates :name, {uniqueness: true, length: {maximum: 20,minimum: 2}}
   validates :introduction, {length: {maximum: 50}}
 end
