1a14ecf2f27c26c1b286365fc8621924fa08dfb5
KAZUKI27854 <02kazuki16@gmail.com>
2021-05-02 03:50:56 +0000

    add address form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,17 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+document.addEventListener("turbolinks:load", function(){
+  /* global $ */
+  $(function() {
+    return $('#user_postcode').jpostal({
+      postcode: ['#user_postcode'],
+      address: {
+        '#user_prefecture_code': '%3',
+        '#user_address_city': '%4',
+        '#user_address_street': '%5%6%7',
+      },
+    });
+  });
+});
