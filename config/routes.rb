e7123d255f2ecfaeee0bb7b3aae261a407b7d737
KAZUKI27854 <02kazuki16@gmail.com>
2021-05-02 01:02:33 +0000

    add search form

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   get '/home/about' => 'homes#about'
   post 'follow/:id' => 'relationships#follow', as: 'follow'
   post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow'
-  get 'users/:id/followers_index', to: 'users#followers_index', as: 'followers'
-  get 'users/:id/followed_index', to: 'users#followed_index', as: 'followed'
+  get 'users/:id/followers_index' => 'users#followers_index', as: 'followers'
+  get 'users/:id/followed_index' => 'users#followed_index', as: 'followed'
+  get '/search' => 'searchs#search'
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
